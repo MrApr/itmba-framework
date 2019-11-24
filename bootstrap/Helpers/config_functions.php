e046df3fa566318af3288b7bf66b81d91f310ed4
mr_apr <mamali0938@gmail.com>
2019-11-25 00:05:51 +0330

    Add Config Functions helper in order to read from config files directly

---
+++ bootstrap/Helpers/config_functions.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Get Config Values
+ * @param $key
+ * @return string
+ */
+function config(string $key)
+{
+    //The value that should return
+    $config_val = null;
+
+    //Explode keys by the key.key.key pattern
+    $keys = explode('.',$key);
+
+    //Check if its array and it's countable then start finding value
+    if(is_array($keys) && count($keys))
+    {
+        foreach ($keys as $key => $value)
+        {
+            //Index 0 is for the file name always
+            if($key == 0)
+            {
+                //If Config file exists, include it
+                if(file_exists(APP_ROOT."/config/".$value.".php"))
+                {
+                    //include config file
+                    $config_val = include_once APP_ROOT."/config/".$value.".php";
+                }
+            }
+            else
+            {
+                //Check if included file has this key ! if it hast put it in Config_value variable
+                if(isset($config_val[$value]))
+                {
+                    $config_val = $config_val[$value];
+                }
+            }
+        }
+    }
+
+    //Check if config_value is array or not ! if not return value if yes return null
+    return (!is_array($config_val)) ? $config_val : null;
+}
